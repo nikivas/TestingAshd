7b8583e9b6ca3552e4b169511d694cba2deaa0cb
Nikivas <nikivas97@mail.ru>
2019-12-12 14:36:11 +0300

    ball 180

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container task-wrap">
     <div class="row justify-content-center">
         <div class="card">
-            @if($ball < 170)
+            @if($ball < 180)
                 <div class="card-header" style="text-align: center;">Задания</div>
                 <div class="card-body">
                     @if (session('status'))
